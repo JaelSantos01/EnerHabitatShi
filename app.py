8a43791a567243f14bde63a56f852f48a35d13c6
JaelSantos01 <20223TN110@utez.edu.mx>
2024-05-29 12:57:37 -0600

    Arreglo del codigo de Memo para nuevas graficas

--- app.py
+++ app.py
@@ -8,24 +8,24 @@ import numpy as np
 from iertools.read import read_epw
 from dateutil.parser import parse 
 import configparser
+import pvlib
+import math
+import locale
+from matplotlib.dates import ConciseDateFormatter, AutoDateLocator
+import pytz
+from ehtools.diatipico import *
 
 # Leer la configuración desde un archivo INI
 config = configparser.ConfigParser()
 config.read("lugares.ini")
 lugares = config.sections()
 
-#ruta = './data/Casablanca.epw'
-#zaca = './data/MEX_MOR_Zacatepec.epw'
-#cuerna = './data/MEX_MOR_Cuernavaca.epw'
-dia = "15"
-#read_epw(ruta) #Visualiza el documento que tiene la ruta
-
-
 def cargar_caracteristicas(lugar):
     lugar_config = config[lugar]
     return {
         "lat": lugar_config.getfloat('lat'),
         "lon": lugar_config.getfloat('lon'),
+        "alt": lugar_config.getint('altitude'),
         "epw": lugar_config['f_epw']
     }
 
@@ -40,7 +40,7 @@ def ruta(lugar):
     ciudad = divi[2].replace('.epw', '')
     ruta = f"./data/{pa}_{es}_{ciudad}.epw"
     return ruta
-
+    
 meses_dict = {
     "Enero": "01",
     "Febrero": "02",
@@ -55,11 +55,6 @@ meses_dict = {
     "Noviembre": "11",
     "Diciembre": "12",
 }
-
-#def carga_epw(ruta_epw):
-#    epw = read_epw(ruta_epw, alias=True, year=2024)
-#    return epw
-
 
 app_ui = ui.page_fluid(
     ui.panel_title("Ener-Habitat Phy"),
@@ -79,6 +74,16 @@ app_ui = ui.page_fluid(
                 "Conditional",
                 "Condición:",
                 choices=["Sin aire acondicionado", "Con aire acondicionado"]
+            ),
+            ui.input_select(
+                "option",
+                "Ubicación:",
+                choices=["Techo", "Muro"]
+            ),
+            ui.input_slider(
+                "rango",
+                "Número de Sistemas:",
+                1,5,1
             )
         ),
         ui.panel_main(
@@ -98,18 +103,91 @@ def server(input, output, session):
         ruta_epw = ruta(place)
         epw = read_epw(ruta_epw, year=2024, alias=True)
         mes = meses_dict[input.periodo()]
-        fig, ax = plt.subplots(2, figsize=(10, 3), sharex=True)
-        f1 = parse(f"2024-{mes}-{dia}")
-        f2 = f1 + pd.Timedelta("7D")
 
-        ax[0].plot(epw.To, label="Ta")
-        ax[1].plot(epw.Ig, label="Ig")
-        ax[1].plot(epw.Ib, label="Ib")
-        ax[1].plot(epw.Id, label="Id")
+        caracteristicas = cargar_caracteristicas(place)
+        lat = caracteristicas['lat']
+        lon = caracteristicas['lon']
+        alt = caracteristicas['alt']
 
-        ax[0].set_xlim(f1, f2)
-        ax[0].legend()
-        ax[1].legend()
+        dia = '15'
+        absortancia = 0.3
+        h = 13.
+        # Parámetros de la superficie
+        surface_tilt = 90  # Vertical
+        surface_azimuth = 270  #
+        timezone = pytz.timezone('America/Mexico_City')
+
+        if surface_tilt == 0:
+            LWR = 3.9
+        else:
+            LWR = 0.
+
+        f1 = f'2024-{mes}-{dia} 00:00'
+        f2 = f'2024-{mes}-{dia} 23:59'
+        
+        dia = pd.date_range(start=f1, end=f2, freq='1s',tz=timezone)
+        location = pvlib.location.Location(latitude = lat, 
+                                        longitude=lon, 
+                                        altitude=alt,
+                                        tz=timezone,
+                                        name=place)
+
+        dia = location.get_solarposition(dia)
+        del dia['apparent_zenith']
+        del dia['apparent_elevation']
+
+        sunrise,_ = get_sunrise_sunset_times(dia)
+        tTmax,Tmin,Tmax = calculate_tTmaxTminTmax(ruta_epw,mes,epw)
+        
+        # # Calcular la temperatura ambiente y agregarla al DataFrame
+        dia = temperature_model(dia, Tmin, Tmax, sunrise, tTmax)
+        # # Agrega Ig, Ib, Id a dia 
+
+        dia = add_IgIbId_Tn(ruta_epw,dia,epw,mes,f1,f2,timezone)
+
+        total_irradiance = pvlib.irradiance.get_total_irradiance(
+            surface_tilt=surface_tilt,
+            surface_azimuth=surface_azimuth,
+            dni=dia['Ib'],
+            ghi=dia['Ig'],
+            dhi=dia['Id'],
+            solar_zenith=dia['zenith'],
+            solar_azimuth=dia['azimuth']
+        )
+        dia['Is'] = total_irradiance.poa_global
+        dia['Tsa'] = dia.Ta + dia.Is*absortancia/h - LWR
+        DeltaTa= dia.Ta.max() - dia.Ta.min()
+
+        dia['DeltaTn'] = calculate_DtaTn(DeltaTa)
+
+        fig, ax = plt.subplots(2,figsize=(10,6),sharex=True)
+
+        df = dia.iloc[::600]
+        ax[0].plot(df.Ta, 'k-',label='Ta')
+        # ax[0].plot(df.Tn, 'g-',label='Tn')
+        ax[0].plot(df.Tsa,'r-',label='Tsa')
+        ax[0].fill_between(df.index,
+                        df.Tn + df.DeltaTn,
+                        df.Tn - df.DeltaTn,color='green',alpha=0.3)
+
+        ax[1].plot(df.Ig,label='Ig')
+        ax[1].plot(df.Ib,label='Ib')
+        ax[1].plot(df.Id,label='Id')
+        ax[1].plot(df.Is,label='Is')
+
+        ax[0].set_ylabel('Temperatura [$^oC$]')
+        ax[1].set_ylabel('Irradiancia [$W/m^2$]')
+
+        locator = AutoDateLocator()
+        formatter = ConciseDateFormatter(locator)
+        ax[1].xaxis.set_major_formatter(formatter)
+
+
+        for a in ax:
+            a.legend()
+            a.grid()
+        fig.tight_layout()
+
         return fig
 
     @output
@@ -119,8 +197,9 @@ def server(input, output, session):
         caracteristicas = cargar_caracteristicas(lugar)
         lat = caracteristicas['lat']
         lon = caracteristicas['lon']
+        alt = caracteristicas['alt']
         epw = caracteristicas['epw']
-        return f"Latitud: {lat}, Longitud: {lon}, EPW: {epw}"
+        return f"Latitud: {lat}, Longitud: {lon}, Altitud: {alt}, EPW: {epw}"
     
 # Crear la aplicación de Shiny
 app = App(app_ui, server)
---
+++ ehtools/diatipico.py
@@ -0,0 +1,131 @@
+import pvlib
+import pandas as pd
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from iertools.read import read_epw
+from dateutil.parser import parse
+import locale
+from matplotlib.dates import ConciseDateFormatter, AutoDateLocator
+import pytz
+from . import dia
+
+def temperature_model(df, Tmin, Tmax, Ho, Hi):
+    """
+    Calcula la temperatura ambiente y agrega una columna 'Ta' al DataFrame.
+
+    Parameters:
+    df (pd.DataFrame): DataFrame con la columna 'index' que representa los tiempos.
+    Tmin (float): Temperatura mínima.
+    Tmax (float): Temperatura máxima.
+    Ho (float): Hora de amanecer (en horas).
+    Hi (float): Hora de m'axima temperatura (en horas).
+
+    Returns:
+    pd.DataFrame: DataFrame con una nueva columna 'Ta' que contiene la temperatura ambiente.
+    """
+    Ho_sec = Ho * 3600
+    Hi_sec = Hi * 3600
+    day_hours = 24 * 3600
+    times = pd.to_datetime(df.index)
+    y = np.zeros(len(times))
+    
+    for i, t in enumerate(times):
+        t_sec = t.hour * 3600 + t.minute * 60 + t.second
+        if t_sec <= Ho_sec:
+            y[i] = (math.cos(math.pi * (Ho_sec - t_sec) / (day_hours + Ho_sec - Hi_sec)) + 1) / 2
+        elif Ho_sec < t_sec <= Hi_sec:
+            y[i] = (math.cos(math.pi * (t_sec - Ho_sec) / (Hi_sec - Ho_sec)) + 1) / 2
+        else:
+            y[i] = (math.cos(math.pi * (day_hours + Ho_sec - t_sec) / (day_hours + Ho_sec - Hi_sec)) + 1) / 2
+
+    Ta = Tmin + (Tmax - Tmin) * (1 - y)
+    df['Ta'] = Ta
+    return df
+
+# Función para calcular Ho y Hi
+def get_sunrise_sunset_times(df):
+    sunrise_time = df[df['elevation'] >= 0].index[0]
+    sunset_time = df[df['elevation'] >= 0].index[-1]
+    
+    Ho = sunrise_time.hour + sunrise_time.minute / 60
+    Hi = sunset_time.hour + sunset_time.minute / 60
+    
+    return Ho, Hi
+    
+def calculate_tTmaxTminTmax(f_epw,mes,epw):
+    # epw = read_epw(f_epw,alias=True,year='2024')
+    epw_mes = epw.loc[epw.index.month==int(mes)]
+    hora_minutos = epw_mes.resample('D').To.idxmax()
+    hora = hora_minutos.dt.hour
+    minuto = hora_minutos.dt.minute
+    tTmax = hora.mean() +  minuto.mean()/60 
+    epw_mes = epw.loc[epw.index.month==int(mes)]
+    horas  = epw_mes.resample('D').To.idxmax().resample('ME').mean().dt.hour 
+    minutos = epw_mes.resample('D').To.idxmax().resample('ME').mean().dt.minute
+    tTmax = horas.iloc[0]+ minutos.iloc[0]/60 
+    Tmin =  epw_mes.resample('D').To.min().resample('ME').mean().iloc[0]
+    Tmax =  epw_mes.resample('D').To.max().resample('ME').mean().iloc[0]
+    
+    return tTmax,Tmin,Tmax
+
+
+
+def add_IgIbId_Tn(f_epw,dia,epw,mes,f1,f2,timezone):
+    # epw = read_epw(f_epw,alias=True,year='2024')
+    epw_mes = epw.loc[epw.index.month==int(mes)]
+    Irr = epw_mes.groupby(by=epw_mes.index.hour)[['Ig','Id','Ib']].mean()
+    tiempo = pd.date_range(start=f1, end=parse(f2), freq='1h',tz=timezone)
+    Irr.index = tiempo
+    Irr = Irr.resample('1s').interpolate(method='time')
+    dia['Ig'] = Irr.Ig
+    dia['Ib'] = Irr.Ib
+    dia['Id'] = Irr.Id
+    dia.ffill(inplace=True)
+    dia['Tn'] = 13.5 + 0.54*dia.Ta.mean()
+    
+    return dia
+    
+def calculate_DtaTn(Delta):
+    if Delta < 13:
+        tmp2 = 2.5 / 2
+    elif 13 <= Delta < 16:
+        tmp2 = 3.0 / 2
+    elif 16 <= Delta < 19:
+        tmp2 = 3.5 / 2
+    elif 19 <= Delta < 24:
+        tmp2 = 4.0 / 2
+    elif 24 <= Delta < 28:
+        tmp2 = 4.5 / 2
+    elif 28 <= Delta < 33:
+        tmp2 = 5.0 / 2
+    elif 33 <= Delta < 38:
+        tmp2 = 5.5 / 2
+    elif 38 <= Delta < 45:
+        tmp2 = 6.0 / 2
+    elif 45 <= Delta < 52:
+        tmp2 = 6.5 / 2
+    elif Delta >= 52:
+        tmp2 = 7.0 / 2
+    else:
+        tmp2 = 0  # Opcional, para cubrir cualquier caso no contemplado, aunque el rango anterior es exhaustivo
+
+    return tmp2
+
+
+
+def calculate_dt(df):
+    """
+    Agrega una columna 'Delta_t' al DataFrame donde se calcula el intervalo de tiempo
+    entre elementos consecutivos del índice en segundos.
+    """
+    
+    # Calcular la diferencia de tiempo entre índices consecutivos
+    delta_t = df.index.to_series().diff().dt.total_seconds()
+    
+    # Agregar la columna 'Delta_t' al DataFrame
+    df['dt'] = delta_t
+    df.dt = df.dt.bfill()
+    return df
+
+
