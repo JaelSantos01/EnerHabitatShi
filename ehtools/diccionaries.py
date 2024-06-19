05914a614392a346de8f875656de9b68ddec9617
JaelSantos01 <20223tn110@utez.edu.mx>
2024-06-19 11:28:54 -0600

    codigo depurado

---
+++ ehtools/diccionaries.py
@@ -0,0 +1,53 @@
+meses_dict = {
+    "Enero": "01",
+    "Febrero": "02",
+    "Marzo": "03",
+    "Abril": "04",
+    "Mayo": "05",
+    "Junio": "06",
+    "Julio": "07",
+    "Agosto": "08",
+    "Septiembre": "09",
+    "Octubre": "10",
+    "Noviembre": "11",
+    "Diciembre": "12",
+}
+
+location={
+    "Muro": 90,
+    "Techo": 0,
+}
+
+orientacion = {
+    "Norte": 0,
+    "Noreste": 45,
+    "Este": 90, 
+    "Sureste": 135, 
+    "Sur": 180,
+    "Suroeste": 225, 
+    "Oeste": 270, 
+    "Noroeste": 315,
+}
+
+Absorbance = {
+    "Aluminio pulido": 0.10,
+    "Aluminio oxidado": 0.15,
+    "Impermeabilizante o pintura blanca nueva": 0.15,
+    "Impermeabilizante o pintura blanca": 0.20,
+    "Pintura aluminio": 0.2,
+    "Lámina galvanizada brillante": 0.25,
+    "Pintura colores claros": 0.3,
+    "Recubrimiento elastomérico blanco": 0.30,
+    "Acero" : 0.45,
+    "Pintura colores intermedios": 0.50,
+    "Concreto claro o adocreto claro": 0.60,
+    "Ladrillo rojo": 0.65,
+    "Impermeabilizante rojo terracota": 0.70,
+    "Lámina galvanizada": 0.7,
+    "Pintura colores oscuros": 0.7,
+    "Teja roja": 0.7,
+    "Concreto": 0.7,
+    "Impermeabilizante o pintura negra": 0.90,
+    "Asfalto nuevo": 0.95,
+    "Impermeabilizante o pintura negra mate nueva": 0.95,
+}
