d0c026745bd5d01b3d4145bc1abef0ca6eedc9e5
JaelSantos01 <20223TN110@utez.edu.mx>
2024-06-27 11:59:43 -0600

    visible_modal

--- app.py
+++ app.py
@@ -3,51 +3,9 @@ from shiny import App, ui, reactive, Session
 
 from utils.helper_text import info_modal
 
-page_dependencies = ui.tags.head(
-    ui.tags.link(rel="stylesheet", type="text/css", href="layout.css"),
-    ui.tags.link(rel="stylesheet", type="text/css", href="style.css"),
-    ui.tags.script(src="index.js"),
-
-    # PWA Support
-    ui.tags.script("""
-
-        async function delayManifest() {
-            let retries = 100;
-            let statusCode = 404;
-            let response;
-
-            while (statusCode === 404 && --retries > 0) {
-                response = await fetch("pwa/manifest.json");
-                statusCode = response.statusCode;
-            }
-
-            if (response.statusCode === 404) throw new Error('max retries reached');
-
-            $('head').append('<link rel="manifest" href="pwa/manifest.json"/>');
-
-            return response;
-        }
-        delayManifest();
-
-        if('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('/respiratory_disease_pyshiny/pwa-service-worker.js', { scope: '/respiratory_disease_pyshiny/' })
-            .then(function() { console.log('Service Worker Registered'); });
-        }
-    """),
-    ui.tags.link(rel="apple-touch-icon", href="pwa/icon.png"),
-
-    ui.tags.meta(name="description", content="Respiratory Disease PyShiny"),
-    ui.tags.meta(name="theme-color", content="#000000"),
-    ui.tags.meta(name="apple-mobile-web-app-status-bar-style", content="#000000"),
-    ui.tags.meta(name="apple-mobile-web-app-capable", content="yes"),
-    ui.tags.meta(name="viewport", content="width=device-width, initial-scale=1"),
-)
-
 
 
 app_ui = ui.page_fluid(
-    page_dependencies,
     title="Respiratory Disease App",
 )
 
--- utils/helper_text.py
+++ utils/helper_text.py
@@ -1,50 +1,6 @@
 from shiny.ui import modal_show, modal, modal_button
 from htmltools import TagList, tags
 
-about_text = TagList(
-    tags.h3("About"),
-    tags.br(),
-    tags.p(
-        """
-        The app gives a visual overview of PM2.5 air pollution
-        for different
-        countries over the years and its potential relationship
-        to respiratory
-        diseases and their prevalence.
-        """,
-        style="""
-        text-align: justify;
-        word-break:break-word;
-        hyphens: auto;
-        """,
-    ),
-)
-
-slider_text_map = tags.p(
-    """
-    Please use the slider below to choose the year. The map will
-    reflect data for the input
-    """,
-    style="""
-    text-align: justify;
-    word-break:break-word;
-    hyphens: auto;
-    """,
-)
-
-slider_text_plot = tags.p(
-    """
-    Please use the slider below to change the years as well as the
-    dropdown to select the countries to compare. By default, the mean
-    data for the World is plotted.
-    """,
-    style="""
-    text-align: justify;
-    word-break:break-word;
-    hyphens: auto;
-    """,
-)
-
 dataset_information = TagList(
     tags.strong(tags.h3("Dataset Information")),
     tags.p(
